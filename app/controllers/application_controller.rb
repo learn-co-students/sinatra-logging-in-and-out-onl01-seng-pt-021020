2e7a17d8ea1496c554e301370494e4f6b2b5112c
jkatem <jenmiao@gmail.com>
2020-05-13 11:49:18 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,25 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
 
-  end
+    @user = User.find_by(:username => params[:username])
+
+    if @user && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else 
+      erb :error 
+    end 
+  end 
+    
 
   get '/account' do
+    erb :account
 
   end
 
   get '/logout' do
+    session.clear
+    redirect to '/'
 
   end
 
