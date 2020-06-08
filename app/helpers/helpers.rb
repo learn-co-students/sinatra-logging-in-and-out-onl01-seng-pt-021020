a4c851ca27ca2e917dd72cd0424c8582548b103b
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-08 15:36:48 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
+require 'sinatra/base'
+
 class Helpers
+  def self.current_user(session_hash)
+    if session_hash[:user_id]
+       User.find(session_hash[:user_id])
+     else
+       nil
+     end
+  end
+
+  def self.is_logged_in?(session_hash)
+    !!current_user(session_hash)
+  end
 end
